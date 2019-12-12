9b9ad7aad4ac447f7e1680775c44fb1d62afac62
Krzysztof Lubiniecki <klubiniecki@gmail.com>
2019-12-12 15:14:44 +0100

    add /:id route for single movie

--- router/router.js
+++ router/router.js
@@ -3,18 +3,30 @@ import Movie from "../models/movie";
 import getFilters from "../filters/getFilters";
 
 const router = express.Router();
+const fields = "title genres fullplot poster year";
 
 router.get("/", async ({ query }, res) => {
   try {
-    const fields = "title genres fullplot poster year";
     const movies = await Movie.find(getFilters(query), fields).sort({
       year: "asc"
     });
 
-    const notFoundMessage = `No movies found for year: ${query["year"]}!`;
-
     if (movies.length < 1) {
-      res.json({ message: notFoundMessage });
+      res.json({ message: `No movies found for year: ${query["year"]}!` });
+    }
+
+    res.json(movies);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get("/:id", async ({ params }, res) => {
+  try {
+    const movies = await Movie.findById(params.id, fields);
+
+    if (!movies) {
+      res.json({ message: `No movie found for id: ${params.id}!` });
     }
 
     res.json(movies);
